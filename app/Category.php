2e8fcdc8a49b9e4edf809f42cb5bb880d194c5d1
TimothyMee <timothy33.tf@gmail.com>
2019-10-10 02:55:23 +0100

    wrote crud for Category and Hobby model

--- app/Category.php
+++ app/Category.php
@@ -3,10 +3,37 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
 
 class Category extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         "name", "description", "created_by"
     ];
+
+    public function createNew($data)
+    {
+        $data['created_by'] = auth()->id();
+        return $this->create($data);
+    }
+
+    public function viewAll()
+    {
+        return $this->all();
+    }
+
+    public function viewOne($data)
+    {
+        return $this->where('name', $data['name'])
+                    ->orWhere('id', $data['id'])
+                    ->get();
+    }
+
+    public function deleteOne($data)
+    {
+        return $this->where('id', $data['id'])->delete();
+    }
 }
--- app/Hobby.php
+++ app/Hobby.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hobby extends Model
 {
+    use SoftDeletes;
+
     protected $fillable = [
         "user_id", "name", "category", "description"
     ];
@@ -18,5 +21,37 @@ class Hobby extends Model
     public function user()
     {
         return $this->belongsTo('App\User');
+    }
+
+    public function createNew($data)
+    {
+        $data['user_id'] = auth()->id();
+        return $this->create($data);
+    }
+
+    public function viewAll()
+    {
+        return $this->with(['category', 'user'])->get();
+    }
+
+    public function viewOne($data)
+    {
+        return $this->where('name', $data['name'])
+                    ->orWhere('id', $data['id'])
+                    ->where('user_id', auth()->id())
+                    ->with(['category','user'])
+                    ->get();
+    }
+
+    public function deleteOne($data)
+    {
+        return $this->where('id', $data['id'])->delete();
+    }
+
+    public function update($data)
+    {
+        return $this->where('id', $data['id'])
+                    ->fill($data)
+                    ->save();
     }
 }
