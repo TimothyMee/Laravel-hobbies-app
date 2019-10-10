64d49569c783b73f85dc5a9f148029d94714bf6a
Timothy_Mee <timothy33.tf@gmail.com>
2019-10-10 18:25:36 +0100

    - added Helpers to help format json response - Wrote the crud functionality for hobbies

--- app/Http/Controllers/HobbyController.php
+++ app/Http/Controllers/HobbyController.php
@@ -2,9 +2,67 @@
 
 namespace App\Http\Controllers;
 
+use App\Hobby;
 use Illuminate\Http\Request;
 
 class HobbyController extends Controller
 {
-    //
+    public function createNew(Request $request, Hobby $hobby)
+    {
+        try {
+            $validatedData = $request->validate([
+                'name' => 'required',
+                'category' => 'required',
+                'description' => 'required'
+            ]);
+
+            $result = $hobby->createNew($validatedData);
+            return apiSuccess($result);
+        } catch (\Exception $e) {
+            return apiFailure($e->getMessage());
+        }
+    }
+
+    public function getAll(Hobby $hobby)
+    {
+        try {
+            $results = $hobby->viewAll();
+            return apiSuccess($results);
+        } catch (\Exception $e) {
+            return apiFailure($e->getMessage());
+        }
+    }
+
+    public function get(Request $request, Hobby $hobby)
+    {
+        try {
+            $data = $request->all();
+            $result = $hobby->viewOne($data);
+            return apiSuccess($result);
+        } catch (\Exception $e) {
+            return apiFailure($e->getMessage());
+        }
+    }
+
+    public function updateHobby(Request $request, Hobby $hobby)
+    {
+        try {
+            $data = $request->all();
+            $result = $hobby->update($data);
+            return apiSuccess($result);
+        } catch (\Exception $e) {
+            return apiFailure($e->getMessage());
+        }
+    }
+
+    public function deletehobby(Request $request, Hobby $hobby)
+    {
+        try {
+            $data = $request->all();
+            $result = $hobby->deleteOne($data);
+            return apiSuccess($result);
+        } catch (\Exception $e) {
+            return apiFailure($e->getMessage());
+        }
+    }
 }
